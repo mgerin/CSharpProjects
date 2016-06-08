5a2d2617c70076586a71ed084a8e385c64c4e5b1
mgerin <marin_gerin@abv.bg>
2016-06-08 17:09:24 +0300

    New projects

---
+++ DataTypesExercises/17.DifferentIntegersSize/DifferentIntegersSize.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _17.DifferentIntegersSize
+{
+    class DifferentIntegersSize
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+            long l;
+            if (long.TryParse(input, out l))
+            {
+                Console.WriteLine($"{input} can fit in:");
+                sbyte sb;
+                byte b;
+                short sh;
+                ushort ush;
+                int i;
+                uint ui;
+                if (sbyte.TryParse(input, out sb)) { Console.WriteLine("* sbyte"); }
+                if (byte.TryParse(input, out b)) { Console.WriteLine("* byte"); }
+                if (short.TryParse(input, out sh)) { Console.WriteLine("* short"); }
+                if (ushort.TryParse(input, out ush)) { Console.WriteLine("* ushort"); }
+                if (int.TryParse(input, out i)) { Console.WriteLine("* int"); }
+                if (uint.TryParse(input, out ui)) { Console.WriteLine("* uint"); }
+                Console.WriteLine("* long");
+            }
+            else
+            {
+                Console.WriteLine($"{input} can't fit in any type");
+            }
+        }
+    }
+}
---
+++ DataTypesExercises/18.HelloName!/HelloName.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _18.HelloName_
+{
+    class HelloName
+    {
+        static void Main()
+        {
+            string name = Console.ReadLine();
+            PrintName(name);
+        }
+
+        private static void PrintName(string name)
+        {
+            Console.WriteLine("Hello, {0}!", name);
+        }
+    }
+}
