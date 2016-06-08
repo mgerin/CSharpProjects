47a8d23d6858626c76042bcecc39b467089701a4
mgerin <marin_gerin@abv.bg>
2016-06-08 17:14:16 +0300

    Exam April
    problems 01 and 02

---
+++ ProgrammingFundamentalsExamApril/01.SweetDessert/SweetDessert.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _01.SweetDessert
+{
+    class SweetDessert
+    {
+        static void Main()
+        {
+            double cash = double.Parse(Console.ReadLine());
+            int guests = int.Parse(Console.ReadLine());
+            double bananasPrice = double.Parse(Console.ReadLine());
+            double eggsPrice = double.Parse(Console.ReadLine());
+            double berriesPrice = double.Parse(Console.ReadLine());
+
+            int sets = guests / 6;
+            if (guests % 6 != 0)
+            {
+                sets++;
+            }
+
+            double productPrice = sets * (2 * bananasPrice + 4 * eggsPrice + 0.20 * berriesPrice);
+
+            if (cash >= productPrice)
+            {
+                Console.WriteLine("Ivancho has enough money - it would cost {0:F2}lv.", productPrice);
+            }
+            else
+            {
+                Console.WriteLine("Ivancho will have to withdraw money - he will need {0:F2}lv more.", productPrice - cash);
+            }
+        }
+    }
+}
---
+++ ProgrammingFundamentalsExamApril/02.GameOfNames/GameOfNames.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _02.GameOfNames
+{
+    class GameOfNames
+    {
+        static void Main()
+        {
+            int n = byte.Parse(Console.ReadLine());
+            int totalScore = int.MinValue;
+            string player = String.Empty;
+
+            for (int i = 0; i < n; i++)
+            {
+                string name = Console.ReadLine();
+                int score = int.Parse(Console.ReadLine());
+                for (int j = 0; j < name.Length; j++)
+                {
+                    if ((int) name[j] % 2 == 0)
+                    {
+                        score += (int) name[j];
+                    }
+                    else
+                    {
+                        score -= (int) name[j];
+                    }
+                }
+
+                if (score > totalScore)
+                {
+                    totalScore = score;
+                    player = name;
+                }
+            }
+
+            Console.WriteLine("The winner is {0} - {1} points", player, totalScore);
+        }
+    }
+}
