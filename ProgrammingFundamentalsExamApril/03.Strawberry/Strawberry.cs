86cab60935b7fe1d0d8415f4b74a45366c47e7aa
mgerin <marin_gerin@abv.bg>
2016-06-09 16:43:14 +0300

    New projects

---
+++ ProgrammingFundamentalsExamApril/03.Strawberry/Strawberry.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _03.Strawberry
+{
+    class Strawberry
+    {
+        static void Main()
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < number / 2; i++)
+            {
+                Console.WriteLine("{0}\\{1}|{1}/{0}", new string('-', i * 2), new string('-', number - i * 2));
+            }
+
+            int dots = 1;
+
+            for (int i = 0; i < number / 2 + 1; i++)
+            {
+                Console.WriteLine("{0}#{1}#{0}", new string('-', number - i * 2), new string('.', dots));
+                dots += 4;
+            }
+
+            int width = number*2 + 3;
+
+            for (int i = 0; i <= number; i++)
+            {
+                Console.WriteLine("{0}#{1}#{0}", new string('-', i), new string('.', width - 2 - 2 * i));
+            }
+        }
+    }
+}
---
+++ ProgrammingFundamentalsExamApril/04.ArrayModifier/ArrayModifier.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+
+namespace _04.ArrayModifier
+{
+    class ArrayModifier
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+            long[] arr = input.Split().Select(n => Convert.ToInt64(n)).ToArray();
+
+            string commands = Console.ReadLine();
+
+            while (commands != "end")
+            {
+                string[] command = commands.Split();
+                if (command[0] == "swap")
+                {
+                    int indexOne = int.Parse(command[1]);
+                    int indexTwo = int.Parse(command[2]);
+                    long temp = arr[indexTwo];
+                    arr[indexTwo] = arr[indexOne];
+                    arr[indexOne] = temp;
+                }
+                else if (command[0] == "multiply")
+                {
+                    int indexOne = int.Parse(command[1]);
+                    int indexTwo = int.Parse(command[2]);
+                    
+                    arr[indexOne] = arr[indexOne] * arr[indexTwo];
+                }
+                else
+                {
+                    for (int i = 0; i < arr.Length; i++)
+                    {
+                        arr[i]--;
+                    }
+                }
+
+                commands = Console.ReadLine();
+            }
+
+            Console.WriteLine(string.Join(", ", arr));
+        }
+    }
+}
